126b038a515830cf11bad28a2b8e695019d72992
Ed Lee <ed@thefamilee.co.uk>
2026-01-24 15:55:18 +0000

    Revert "Added -v & -h cli switches"
    This reverts commit 643ebd9e1b2b4a3e573e25dd1f8ddfd1d599ef76.

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,13 @@
-
 #include <iostream>
 #include "Config.hpp"
 #include "Bridge.hpp"
 #include <thread>
 #include <chrono>
-#include <cstring>
-#include "../build/VersionInfo.h"
 
 int main(int argc, char** argv) {
-
-    // CLI argument parsing
-    auto print_help = []() {
-        std::cout << "dbus-mqtt-bridge - Bi-directional bridge between DBus and MQTT\n"
-                  << "\nUsage: dbus-mqtt-bridge [config.yaml]\n"
-                  << "\nOptions:\n"
-                  << "  -h, --help      Show this help message and exit\n"
-                  << "  -v, --version   Show program version and exit\n"
-                  << "\nBy default, uses 'config.yaml' in the current directory.\n"
-                  << "See the man page (man dbus-mqtt-bridge) for full documentation.\n";
-    };
-
-    for (int i = 1; i < argc; ++i) {
-        if (std::strcmp(argv[i], "-h") == 0 || std::strcmp(argv[i], "--help") == 0) {
-            print_help();
-            return 0;
-        }
-        if (std::strcmp(argv[i], "-v") == 0 || std::strcmp(argv[i], "--version") == 0) {
-            std::cout << "dbus-mqtt-bridge version " << CLIENT_VERSION << " (built " << BUILD_TIMESTAMP << ")\n";
-            return 0;
-        }
-    }
-
     std::string configFile = "config.yaml";
     if (argc > 1) {
-        // Skip switches
-        for (int i = 1; i < argc; ++i) {
-            if (argv[i][0] != '-') {
-                configFile = argv[i];
-                break;
-            }
-        }
+        configFile = argv[1];
     }
 
     try {
